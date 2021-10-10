f1c6c97a57010c8a01ef9ab61c186a3f58632aa1
unknown <josedanielramosbarquero@gmail.com>
2021-10-09 20:52:37 -0600

    arreglo de fonts

--- src/components/serchBar.js
+++ src/components/serchBar.js
@@ -6,7 +6,7 @@ const SearchBar = ({names}) => {
   const [serchTerm, setSerchTerm] = useState('')
   return (
     <div>
-      <Stack spacing={4} w="100%" h="100%" m="0 auto" pt="100px">
+      <Stack spacing={4} w="100%" h="100%" m="0 auto" pt="100px" fontFamily="body">
         <InputGroup w={{ base: "328px", md: "80%", lg: "50%" }} m="0 auto" bg="brand.baseColor">
           <Input variant='outline' placeholder="busqueda Artista" size="lg" w="100%" borderWidth="5px" borderColor="brand.secondaryGray" borderRadius="0" onChange={event => { setSerchTerm(event.target.value) }} />
           <InputRightElement width="15px" mr="15px" mt="5px" children={<BsSearch/>} />
