a22755c6db4c6f2f118efdc88b739cb426e6e763
Fabian Barquero <fabianbarquero9@gmail.com>
2021-10-12 13:23:52 -0600

    Agrego cambios en index

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ChakraProvider, SimpleGrid, Container } from "@chakra-ui/react";
 import Card from "./Card";
+import img from "./img/ana_cristina.png"
+
+ReactDOM.render(<Card/>, document.getElementById('root'));
 
 function App() {
   const dataList = [
     {
       id: "1",
+      img: `${img}`,
       full_name: "Ana Cristina"
     },
     {
       id: "2",
+      img: `${img}`,
       full_name: "Ana Cristina"
     }
   ];
