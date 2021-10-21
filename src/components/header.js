60f73cbe412687a5718969f4ed065233350f2053
unknown <josedanielramosbarquero@gmail.com>
2021-10-21 15:17:23 -0600

    se remplaza headerCronicas por header

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Box, Flex, Spacer, Image } from "@chakra-ui/react"
+import MenuHam from './MenuHam'
+import logo from '../../public/logoNegro.svg'
+
+function Header() {
+    return (
+        <>
+            <Flex bg="brand.primaryOrange" alignItems="center">
+                <Box p={8}>
+                    <Image w={{ base: "42px", md: "50px", lg: "58px" }} src={logo} alt="Cronologias de lo Invisible" />
+                </Box>
+                <Spacer />
+                <MenuHam></MenuHam>
+            </Flex>
+        </>
+    )
+}
+export default Header
