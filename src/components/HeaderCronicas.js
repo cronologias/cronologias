61ef012432864bcd0805cd2e3c928ee2e4803214
unknown <josedanielramosbarquero@gmail.com>
2021-10-21 14:57:45 -0600

    elimino etiqueta image de headercronicas

--- src/components/HeaderCronicas.js
+++ src/components/HeaderCronicas.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Flex, Spacer, Image } from "@chakra-ui/react"
+import { Box, Flex, Spacer } from "@chakra-ui/react"
 import MenuHam from './menuHam'
 
 
@@ -8,7 +8,6 @@ function HeaderCronicas() {
     <>
       <Flex bg="brand.primaryOrange" alignItems="center">
         <Box p={8}>
-          <Image w={{ base: "42px", md: "50px", lg: "58px" }} src='/logoNegro.svg' alt="Cronologias de lo Invisible" />
         </Box>
         <Spacer/>
         <MenuHam/>
