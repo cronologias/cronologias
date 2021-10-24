08c60ba5d23f3161c55b24bc3d853ed7e51b8bc2
Fabian barquero <fabianbarquero9@gmail.com>
2021-10-24 13:59:02 -0600

    Cambio de nombre del cloudName

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -4,7 +4,7 @@ import { Video, CloudinaryContext } from "cloudinary-react";
 const VideoPlayer = () => {
   const videoRef = useRef();
   return (
-    <CloudinaryContext cloud_name="codedog">
+    <CloudinaryContext cloud_name="hola_ihbzhs">
     	<div>
         <Video
           publicId="https://res.cloudinary.com/cronologias-invisible/video/upload/v1634869855/cronologias/360/hola_ihbzhs.mp4"
