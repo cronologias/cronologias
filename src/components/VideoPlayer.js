5027aae5f791cd05657e64558045cce84f274a7d
Fabian barquero <fabianbarquero9@gmail.com>
2021-10-24 14:03:58 -0600

    Add changes

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -4,10 +4,10 @@ import { Video, CloudinaryContext } from "cloudinary-react";
 const VideoPlayer = () => {
   const videoRef = useRef();
   return (
-    <CloudinaryContext cloud_name="hola_ihbzhs">
+    <CloudinaryContext cloud_name="https://res.cloudinary.com/cronologias-invisible/video/upload/v1634869855/cronologias/360/hola_ihbzhs.mp4">
     	<div>
         <Video
-          publicId="https://res.cloudinary.com/cronologias-invisible/video/upload/v1634869855/cronologias/360/hola_ihbzhs.mp4"
+          publicId="hola_ihbzhs"
           width="100%"
           controls
           innerRef={videoRef}
@@ -17,7 +17,15 @@ const VideoPlayer = () => {
   );
 };
 
+cloudinary.videoTag('hola_ihbzhs.mp4').toHtml()
+
 export default VideoPlayer;
 
-// install package: npm i cloudinary
+/* install package: 
+
+npm i cloudinary
+npm i cloudinary-video-player
+npm i claudinary-core
+
+*/
 // References: https://cloudinary.com/blog/how_to_serve_videos_in_next_js_applications_with_cloudinary
