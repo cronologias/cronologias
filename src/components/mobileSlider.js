17c13a74bf9996468a7b3233247c0122fa241912
stephanie-vm <stephaniecetav@gmail.com>
2021-10-28 17:01:26 -0600

    Add changes of routes

--- src/components/mobileSlider.js
+++ src/components/mobileSlider.js
@@ -31,10 +31,10 @@ function MobileSlider(props) {
                     spacing={{ base:"0.5rem", md:"0.6rem", lg:"1.5rem" }}>
                     {props.mobleSlides.map((slide, sid) => (
                         <>
-                            <Link href={`/exposition-museum/${slide.id}`} as={`/exposition-museum/${slide.id}`}>
+                            <Link href={`/exposition-museum/${slide.id}`} as={`/exposition-museum/${slide.id}`} >
                             <a>
-                                <LinkBox key={`slide-card-${sid}`}>
-                                <Box 
+                                <LinkBox >
+                                <Box key={`slide-card-${sid}`}
                                 w={{ base:'14.938rem', md:'18rem', lg:'24rem' }}
                                 h={{ base:'13.75rem', md:'14rem', lg:'16.875rem' }}
                                 borderBottom='4px' 
