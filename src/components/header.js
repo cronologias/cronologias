92a5ab2ab7cd17d41d290579fee737795e011b47
unknown <josedanielramosbarquero@gmail.com>
2021-11-04 15:30:45 -0600

    agrego optimizacion de imagenes

--- src/components/header.js
+++ src/components/header.js
@@ -9,13 +9,13 @@ function Header() {
     return (
         <>
             <Flex bg='brand.primaryOrange' alignItems='center'>
-                <Box p={8} w={{ base: '100px', md: '130px', lg: '150px' }} position='relative' height='100px' ml={8}>
                     <Link href='/'>
                         <a>
+                        <Box p={8} w={{ base: '100px', md: '130px', lg: '150px' }} position='relative' height='100px' ml={8}>
                             <Image layout='fill' src={logo} alt='Cronologias de lo Invisible' />
+                            </Box>
                         </a>
                     </Link>
-                </Box>
                 <Spacer />
                 <MenuHam></MenuHam>
             </Flex>
--- src/pages/documentatio/index.js
+++ src/pages/documentatio/index.js
@@ -1,85 +1,78 @@
-import {Box, HStack, Center, Link} from '@chakra-ui/react'
-import {getApiRes} from "../../services/callApi"
+import { Box, HStack, Center, Link } from '@chakra-ui/react'
+import { getApiRes } from "../../services/callApi"
 import Image from 'next/image'
 import Header from "../../components/header"
 import Footer from "../../components/footer"
 
-function documentation({documentationData}){
-    let url ='/cronologias-invisible'
-    return(
+function documentation({ documentationData }) {
+    let url = '/cronologias-invisible'
+    return (
         <>
-            <Header/>
-            <Box mb={{ base: "2rem", md:"2.25rem", lg:"2.5rem"}}>
+            <Header />
+            <Box mb={{ base: "2rem", md: "2.25rem", lg: "2.5rem" }}>
                 {documentationData.data.allDocumentacions.map((documentation, index) => (
-                    documentation.enlaceDocumentacion === ""? url= url: url = documentation.enlaceDocumentacion,
+                    documentation.enlaceDocumentacion === "" ? url = url : url = documentation.enlaceDocumentacion,
                     index % 2 === 0 ?
-                        <Center 
-                            key={index} 
-                        >
-                            <HStack 
-                                spacing={ {base: 4, md: 6, lg: 8} }
-                                mt={ { base: 5, lg: 8 }} 
-                            >
-                                <Box w={ { base: "11.5rem", md: "28rem", lg: "40rem", xl: "53rem"} }
-                                        h={ {base: "16rem", md: "28rem", lg: "30rem"} } position='relative'>
-                                    <Link href="/" isExternal>
-                                        <a>
-                                            <Image 
-                                                layout='fill'
-                                                objectFit="cover" 
-                                                src={url}
-                                                alt="Documentación">
-                                            </Image>
-                                        </a>
-                                    </Link>
-                                </Box>
+                        <Center
+                            key={index}>
+                            <HStack
+                                spacing={{ base: 4, md: 6, lg: 8 }}
+                                mt={{ base: 5, lg: 8 }}>
+                                <Link href="/">
+                                    <Box w={{ base: "11.5rem", md: "28rem", lg: "40rem", xl: "53rem" }}
+                                        h={{ base: "16rem", md: "28rem", lg: "30rem" }} position='relative'>
+                                        <Image
+                                            layout='fill'
+                                            objectFit="cover"
+                                            src={url}
+                                            alt="Documentación">
+                                        </Image>
+                                    </Box>
+                                </Link>
                                 <Box bg="brand.primaryOrange"
-                                    w={ {base: "4rem",md: "15rem", lg: "14rem", xl: "16rem"} }
-                                    h={ { base: "16rem", md: "28rem", lg: "30rem" }}>
+                                    w={{ base: "4rem", md: "15rem", lg: "14rem", xl: "16rem" }}
+                                    h={{ base: "16rem", md: "28rem", lg: "30rem" }}>
                                 </Box>
                             </HStack>
                         </Center> :
                         <Center key={index} >
-                            <HStack 
+                            <HStack
                                 spacing={{ base: 4, md: 6, lg: 8 }}
-                                mt={{ base: 5, lg: 8 }} 
-                            >
-                                <Box 
+                                mt={{ base: 5, lg: 8 }}>
+                                <Box
                                     bg="brand.primaryBlack"
                                     w={{ base: "4rem", md: "15rem", lg: "14rem", xl: "16rem" }}
                                     h={{ base: "16rem", md: "28rem", lg: "30rem" }} >
                                 </Box>
-                                <Box w={ { base: "11.5rem", md: "28rem", lg: "40rem", xl: "53rem"} }
-                                    h={ {base: "16rem", md: "28rem", lg: "30rem"} } position='relative'>
-                                    <Link href="/">
-                                        <a>
-                                            <Image 
-                                                layout='fill'
-                                                objectFit="cover" 
-                                                src={url}
-                                                alt="Documentación">
-                                            </Image>
-                                        </a>
-                                    </Link>
-                                </Box>
-                        </HStack>
-                    </Center>
-                    ))  
+                                <Link href="/" >
+                                    <Box w={{ base: "11.5rem", md: "28rem", lg: "40rem", xl: "53rem" }}
+                                        h={{ base: "16rem", md: "28rem", lg: "30rem" }} position='relative'>
+                                        <Image
+                                            layout='fill'
+                                            objectFit="cover"
+                                            src={url}
+                                            alt="Documentación">
+                                        </Image>
+                                    </Box>
+                                </Link>
+                            </HStack>
+                        </Center>
+                ))
                 }
             </Box>
-            <Footer/>
+            <Footer />
         </>
     )
 }
 export default documentation
 
 export async function getStaticProps() {
-    const URl= process.env.NEXT_URL;
-    const TOKEN= process.env.NEXT_TOKEN;
-    const bodyDocumentation ='query MyQuery {allDocumentacions {enlaceDocumentacion}}'
-    const getDocumentation = await getApiRes(URl,TOKEN, bodyDocumentation)
+    const URl = process.env.NEXT_URL;
+    const TOKEN = process.env.NEXT_TOKEN;
+    const bodyDocumentation = 'query MyQuery {allDocumentacions {enlaceDocumentacion}}'
+    const getDocumentation = await getApiRes(URl, TOKEN, bodyDocumentation)
     const documentationData = getDocumentation
-    return{
+    return {
         props: {
             documentationData
         }
