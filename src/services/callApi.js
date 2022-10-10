3c27d0cf72e1f756dc6078ed4a4ffbbb01f90e21
José Daniel Ramos Barquero <josedanielramosbarquero@gmail.com>
2022-10-10 16:34:42 -0600

    se corrige el bug que hace que no se muestre la pagina de cada artista

--- src/services/callApi.js
+++ src/services/callApi.js
@@ -1,18 +1,15 @@
-export async function getApiRes (URl, TOKEN, query){
-    const response = await fetch(
-        URl,
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': `Bearer ${TOKEN}`,
-            },
-            body: JSON.stringify({
-                query: query
-            }),
-        }
-    )
-    const resposeJson = await response.json()
-    return resposeJson
+export async function getApiRes(URl, TOKEN, query) {
+	const response = await fetch(URl, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json',
+			Authorization: `Bearer ${TOKEN}`,
+		},
+		body: JSON.stringify({
+			query: query,
+		}),
+	});
+	const resposeJson = await response.json();
+	return resposeJson;
 }
